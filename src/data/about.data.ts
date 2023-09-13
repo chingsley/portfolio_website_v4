74e97146722162a816938839ecdf464469819136
Kingsley Eneja <kingsley.eneja@getir.com>
2023-09-13 21:08:19 +0100

    Deployment setup, September 13, 2023

--- src/data/about.data.ts
+++ src/data/about.data.ts
@@ -1,5 +1,7 @@
-export default `
+const about = `
 Highly skilled and experienced full-stack software engineer with over 6 years of
 professional experience working in both backend and frontend roles. Proficient in Golang,
 Node.js/TypeScript, React, Redux, AWS, PostgreSQL, MySQL, MongoDB, and microservice architecture.
 `;
+
+export default about;
